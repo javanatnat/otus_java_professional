e30bcb17a4550ee7a741249fcb0244dca4b51bbf
Natalia Ivandaeva <natalya.ivandaeva@gmail.com>
2022-02-02 15:46:14 +0700

    add hw25

--- hw25-di/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
+++ hw25-di/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
@@ -1,8 +1,12 @@
 package ru.otus.appcontainer;
 
+import ru.otus.appcontainer.api.AppComponent;
 import ru.otus.appcontainer.api.AppComponentsContainer;
 import ru.otus.appcontainer.api.AppComponentsContainerConfig;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.*;
 
 public class AppComponentsContainerImpl implements AppComponentsContainer {
@@ -16,7 +20,19 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
 
     private void processConfig(Class<?> configClass) {
         checkConfigClass(configClass);
-        // You code here...
+
+        try {
+            Object config = getConfigInstance(configClass);
+            List<Method> methods = getConfigMethods(configClass);
+
+            for (Method m : methods) {
+                processMethod(m, config);
+            }
+
+        } catch (NoSuchMethodException | InvocationTargetException
+                | InstantiationException | IllegalAccessException e) {
+            throw new RuntimeException();
+        }
     }
 
     private void checkConfigClass(Class<?> configClass) {
@@ -25,13 +41,77 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
         }
     }
 
+    private Object getConfigInstance(Class<?> configClass) throws
+            InvocationTargetException,
+            InstantiationException,
+            IllegalAccessException,
+            NoSuchMethodException
+    {
+        Constructor<?> constructor = configClass.getConstructor();
+        return constructor.newInstance();
+    }
+
+    private List<Method> getConfigMethods(Class<?> configClass) {
+        Method[] methods = configClass.getDeclaredMethods();
+        return Arrays.stream(methods)
+                .filter(m -> methodIsComponent(m))
+                .sorted(getComponentComparator())
+                .toList();
+    }
+
+    private static boolean methodIsComponent(Method m) {
+        return m.isAnnotationPresent(AppComponent.class);
+    }
+
+    private Comparator<Method> getComponentComparator() {
+        return Comparator.comparingInt(m -> m.getAnnotation(AppComponent.class).order());
+    }
+
+    private void processMethod(Method method, Object config) throws
+            InvocationTargetException,
+            IllegalAccessException
+    {
+        Class<?>[] paramClasses = method.getParameterTypes();
+        Object[] parameters = getMethodParameters(paramClasses);
+        Object component = method.invoke(config, parameters);
+        setComponent(component, method);
+    }
+
+    private Object[] getMethodParameters(Class<?>[] paramClasses) {
+        int size = paramClasses.length;
+        Object[] parameters = new Object[size];
+
+        for (int i = 0; i < size; i++) {
+            Object object = getAppComponent(paramClasses[i]);
+            if (object == null) throw new RuntimeException();
+            parameters[i] = object;
+        }
+
+        return parameters;
+    }
+
+    private void setComponent(Object component, Method method) {
+        appComponents.add(component);
+        appComponentsByName.put(getComponentName(method), component);
+    }
+
+    private String getComponentName(Method method) {
+        return method.getAnnotation(AppComponent.class).name();
+    }
+
     @Override
     public <C> C getAppComponent(Class<C> componentClass) {
+        for( Object component : appComponents ) {
+            if (componentClass.isInstance(component)) {
+                return componentClass.cast(component);
+            }
+        }
         return null;
     }
 
     @Override
     public <C> C getAppComponent(String componentName) {
-        return null;
+        @SuppressWarnings("unchecked") C component = (C) appComponentsByName.get(componentName);
+        return component;
     }
 }
