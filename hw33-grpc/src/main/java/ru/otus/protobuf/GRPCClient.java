052047d10f162cb6b61dd4a020571de9d3a5f2b8
Natalia Ivandaeva <natalya.ivandaeva@gmail.com>
2022-03-10 11:49:02 +0700

    add hw 33

---
+++ hw33-grpc/src/main/java/ru/otus/protobuf/GRPCClient.java
@@ -0,0 +1,60 @@
+package ru.otus.protobuf;
+
+import io.grpc.ManagedChannelBuilder;
+import ru.otus.protobuf.client.GetValuesObserver;
+import ru.otus.protobuf.generated.ClientRequest;
+import ru.otus.protobuf.generated.RemoteDBServiceGrpc;
+
+import java.util.concurrent.CountDownLatch;
+
+public class GRPCClient {
+
+    private static final String SERVER_HOST = "localhost";
+    private static final int SERVER_PORT = 8190;
+
+    private static final int FIRST_VALUE = 0;
+    private static final int LAST_REQUEST_VALUE = 30;
+    private static final int LAST_LOOP_VALUE = 50;
+    private static final int SLEEP_MILLIS = 1000;
+
+    public static void main(String[] args) throws InterruptedException {
+        var channel = ManagedChannelBuilder.forAddress(SERVER_HOST, SERVER_PORT)
+                .usePlaintext()
+                .build();
+
+        var latch = new CountDownLatch(1);
+        var stub = RemoteDBServiceGrpc.newStub(channel);
+
+        var request = ClientRequest.newBuilder()
+                .setFirstValue(FIRST_VALUE)
+                .setLastValue(LAST_REQUEST_VALUE)
+                .build();
+
+        int currentValue = FIRST_VALUE;
+
+        var observer = new GetValuesObserver(currentValue, latch);
+        stub.getValues(request, observer);
+
+        int oldServerValue = FIRST_VALUE;
+
+        for (int i = FIRST_VALUE; i < LAST_LOOP_VALUE; i++) {
+            try {
+                Thread.sleep(SLEEP_MILLIS);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            int currentServerValue = observer.getCurrentValue();
+            if (currentServerValue != oldServerValue) {
+                currentValue += currentServerValue;
+                oldServerValue = currentServerValue;
+            }
+            currentValue++;
+
+            System.out.println("(iteration=" + ( i + 1 ) + ") current value: " + currentValue);
+        }
+
+        latch.await();
+        channel.shutdown();
+    }
+}
---
+++ hw33-grpc/src/main/java/ru/otus/protobuf/client/GetValuesObserver.java
@@ -0,0 +1,41 @@
+package ru.otus.protobuf.client;
+
+import io.grpc.stub.StreamObserver;
+import ru.otus.protobuf.generated.ServerResponse;
+
+import java.util.concurrent.CountDownLatch;
+
+public class GetValuesObserver implements StreamObserver<ServerResponse> {
+    private int currentValue;
+    private final CountDownLatch latch;
+
+    public GetValuesObserver(
+            int currentValue,
+            CountDownLatch latch
+    ) {
+        this.currentValue = currentValue;
+        this.latch = latch;
+    }
+
+    public int getCurrentValue() {
+        return currentValue;
+    }
+
+    @Override
+    public void onNext(ServerResponse value) {
+        currentValue = value.getValue();
+        System.out.println("\tvalue from server: " + currentValue);
+    }
+
+    @Override
+    public void onError(Throwable t) {
+        System.err.println("error getValues: " + t);
+        latch.countDown();
+    }
+
+    @Override
+    public void onCompleted() {
+        System.out.println("end getValues");
+        latch.countDown();
+    }
+}
